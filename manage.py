c24403c3625581cb0d0f8b4472717dbf368cf343
Miguel Grinberg <miguelgrinberg50@gmail.com>
2014-01-05 03:20:35 -0800

    Chapter 17: Traditional hosting (17d)

--- manage.py
+++ manage.py
@@ -11,6 +11,13 @@ from app.models import User, Follow, Role, Permission, Post, Comment
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
+if os.path.exists('.env'):
+    print('Importing environment from .env...')
+    for line in open('.env'):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1]
+
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
